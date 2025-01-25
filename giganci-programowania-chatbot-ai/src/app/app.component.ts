b015701c6dbfbfb03acd1976da6d38072c3a7c79
giganciprogramowaniaml <giganciprogramowaniaML@gmail.com>
2025-01-25 21:00:39 +0000

    feat: add chat ask question section

--- giganci-programowania-chatbot-ai/src/app/app.component.ts
+++ giganci-programowania-chatbot-ai/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
