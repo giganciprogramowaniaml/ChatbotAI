1205dcea8e157657f785da17c78582bf6c0d84da
giganciprogramowaniaml <giganciprogramowaniaML@gmail.com>
2025-01-25 21:50:26 +0000

    feat: chat styles refactor

--- giganci-programowania-chatbot-ai/src/app/app.component.ts
+++ giganci-programowania-chatbot-ai/src/app/app.component.ts
@@ -5,13 +5,16 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet,FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'giganci-programowania-chatbot-ai';
+  askQuestion(question: string) {
+    
+  }
 }
